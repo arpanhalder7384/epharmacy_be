530de9ad9285879e8900c798e2b870a23af6f142
Arpan Halder <arpanhalder7384@gmail.com>
2025-02-26 09:26:57 +0530

    Add card api bug fixed

--- payment-management/controllers/paymentController.js
+++ payment-management/controllers/paymentController.js
@@ -8,20 +8,17 @@ exports.makePayment = async (req, res) => {
         if (!cardId || !nameOnCard || !cardType || !cvv || !expiryDate || !customerId || !amountToPay) {
             return res.status(400).json({ message: "Missing required fields" });
         }
+        if(cardId.length!==16 || ccv.length!==3){
+            return res.status(400).json({ message: "Invalid card details" });
+        }
+        const card = await Payment.find({"customerId": customerId , cardId:cardId, cvv:cvv});
 
-        const newPayment = new Payment({
-            customerId,
-            cardId,
-            nameOnCard,
-            cardType,
-            cvv,
-            expiryDate,
-            amountPaid: amountToPay
-        });
-
-        await newPayment.save();
-        res.status(201).json({ message: "Payment successful", payment: newPayment });
+        if(!card.length){
+            return res.status(400).json({ message: "Invalid Card ID / Customer ID" });
+        }
+        res.status(201).json({ message: "Payment successful", card:card });
     } catch (error) {
+        console.log(error)
         res.status(500).json({ message: "Server Error", error: error.message });
     }
 };
@@ -29,7 +26,9 @@ exports.makePayment = async (req, res) => {
 exports.viewCards = async (req, res) => {
     try {
         const { customerId } = req.params;
-        const cards = await Payment.find({ customerId });
+        const cards = await Payment.find({"customerId": customerId });
+
+        console.log(cards, customerId)
 
         if (!cards.length) {
             return res.status(400).json({ message: "No cards found for this customer." });
@@ -45,6 +44,16 @@ exports.addCard = async (req, res) => {
     try {
         const { cardId, nameOnCard, cardType, cvv, expiryDate, customerId } = req.body;
 
+        if(cardId.length!==16 || cvv.length!==3){
+            return res.status(400).json({ message: "Invalid card details" });
+        }
+
+        const cards = await Payment.find({"cardId": cardId });
+
+        if(cards.length){
+            return res.status(400).json({ message: "Card already present" });
+        }
+
         if (!cardId || !nameOnCard || !cardType || !cvv || !expiryDate || !customerId) {
             return res.status(400).json({ message: "Missing required fields" });
         }
@@ -54,6 +63,7 @@ exports.addCard = async (req, res) => {
         await newCard.save();
         res.status(201).json({ message: "Card added successfully", card: newCard });
     } catch (error) {
+        console.log(error)
         res.status(500).json({ message: "Server Error", error: error.message });
     }
 };
