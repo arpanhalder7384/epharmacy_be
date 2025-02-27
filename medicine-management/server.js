1ace53d7490095049f6c977bdb8879c5bcbbea2f
Arpan Halder <arpanhalder7384@gmail.com>
2025-02-27 14:44:40 +0530

    Added swagger Package

--- medicine-management/server.js
+++ medicine-management/server.js
@@ -3,7 +3,9 @@ const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
 const loggerMiddleware = require("./middleware/logger");
+const swaggerUi=require("swagger-ui-express")
 const medicineRoutes = require("./routes/medicineRoutes");
+const swaggerFile=require("./swagger_output.json")
 
 const app = express();
 connectDB();
@@ -12,6 +14,7 @@ app.use(cors());
 app.use(express.json());
 app.use(loggerMiddleware);
 app.use("/medicine-api", medicineRoutes);
+app.use("/doc", swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 const PORT = process.env.PORT || 5002;
 app.listen(PORT, () => console.log(`Medicine API running on port ${PORT}`));
